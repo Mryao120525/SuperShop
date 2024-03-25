73b208f9ddb73878e0d56bc3961b571bf4a86dfc
unknown <silver111>
2024-03-25 14:13:38 +0800

    first 0

---
+++ TrainingOne/TrainingOne/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainingOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
